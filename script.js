8df8ae555d4b1045d23ad3b28345c615463df0c4
Tomasz <tomaush02@gmail.com>
2023-10-03 11:18:22 +0200

    added fade in text

---
+++ script.js
@@ -0,0 +1,12 @@
+// window.onload = () => {
+//   var h1Element = document.querySelector(".fade-in");
+//   console.log(h1Element);
+//   h1Element.style.opacity = 1;
+// };
+function fadeFun() {
+  var h1Element = document.getElementsByTagName("h1")[0];
+  console.log(h1Element);
+  h1Element.classList.add("fade-in");
+}
+
+document.addEventListener("DOMContentLoaded", fadeFun);
